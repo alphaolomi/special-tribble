0aa14d58f771964ca4223c35ef4c697b604a37d3
Alpha Olomi <alphaolomi@gmail.com>
2021-05-11 10:30:30 +0300

    feat: add db

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
-print("\n[x] Hello world !\n\n")
+
+from tinydb import TinyDB,Query
+from rich import print
+
+db = TinyDB('app.json')
+users_table = db.table('users')
+
+users_table.insert({	
+	'name': 'John Doe',
+	'email' : 'johndoe@example.com',
+	'dob' : '12/12/1997',
+	'create_at' : '12/12/2021'
+
+})
+users_table.insert({	
+	'name': 'Jane Doe',
+	'email' : 'janedoe@example.com',
+	'dob' : '12/08/1997',
+	'create_at' : '12/12/2021'
+
+})
+
+# All
+# print(users_table.truncate())
+print(users_table.all())
